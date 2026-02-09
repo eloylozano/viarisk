329289e177150ba70bb318d4c9297b8eeb79e74a
eloylozano <lozanobarrioseloy@gmail.com>
2026-02-10 00:19:43 +0100

    create dashboard by year

---
+++ web/viarisk/src/data/mappings.js
@@ -0,0 +1,44 @@
+export const MAPPINGS = {
+    MES: {
+        1: "Enero", 2: "Febrero", 3: "Marzo", 4: "Abril", 5: "Mayo", 6: "Junio",
+        7: "Julio", 8: "Agosto", 9: "Septiembre", 10: "Octubre", 11: "Noviembre", 12: "Diciembre"
+    },
+    DIA_SEMANA: {
+        1: "Lunes", 2: "Martes", 3: "Miércoles", 4: "Jueves", 5: "Viernes", 6: "Sábado", 7: "Domingo"
+    },
+    PROVINCIA: {
+        1: "Álava", 2: "Albacete", 3: "Alicante", 4: "Almería", 5: "Ávila", 6: "Badajoz",
+        7: "Baleares", 8: "Barcelona", 9: "Burgos", 10: "Cáceres", 11: "Cádiz", 12: "Castellón",
+        13: "Ciudad Real", 14: "Córdoba", 15: "A Coruña", 16: "Cuenca", 17: "Girona", 18: "Granada",
+        19: "Guadalajara", 20: "Gipuzkoa", 21: "Huelva", 22: "Huesca", 23: "Jaén", 24: "León",
+        25: "Lleida", 26: "La Rioja", 27: "Lugo", 28: "Madrid", 29: "Málaga", 30: "Murcia",
+        31: "Navarra", 32: "Ourense", 33: "Asturias", 34: "Palencia", 35: "Las Palmas", 36: "Pontevedra",
+        37: "Salamanca", 38: "S.C. Tenerife", 39: "Cantabria", 40: "Segovia", 41: "Sevilla", 42: "Soria",
+        43: "Tarragona", 44: "Teruel", 45: "Toledo", 46: "Valencia", 47: "Valladolid", 48: "Bizkaia",
+        49: "Zamora", 50: "Zaragoza", 51: "Ceuta", 52: "Melilla"
+    },
+    TIPO_VIA_NOMBRE: {
+        1: "Autopista Peaje", 2: "Autopista Libre", 3: "Autovía", 4: "Vía Automóviles",
+        5: "Convencional Doble", 6: "Convencional Única", 7: "Vía Servicio", 8: "Enlace",
+        9: "Calle", 10: "Camino", 11: "Recinto", 12: "Vía Ciclista", 13: "Senda Ciclab.", 14: "Otro"
+    },
+    TIPO_ACCIDENTE_NOMBRE: {
+        1: "Frontal", 2: "Fronto-lateral", 3: "Lateral", 4: "Alcance", 5: "Múltiple",
+        6: "Choque Obstáculo", 7: "Atropello Persona", 8: "Atropello Animal", 9: "Vuelco",
+        10: "Caída", 11: "Salida Vía", 12: "Salida Izq (Colisión)", 13: "Salida Izq (Despeñe)",
+        14: "Salida Izq (Vuelco)", 15: "Salida Izq (Otro)", 16: "Salida Der (Colisión)",
+        17: "Salida Der (Despeñe)", 18: "Salida Der (Vuelco)", 19: "Salida Der (Otro)", 20: "Otro"
+    },
+    CONDICION_METEO: {
+        1: "Despejado", 2: "Nublado", 3: "Lluvia débil", 4: "Lluvia fuerte",
+        5: "Granizando", 6: "Nieve", 7: "Se desconoce", 999: "Sin especificar"
+    },
+    CONDICION_ILUMINACION: {
+        1: "Luz del día", 2: "Ocaso (sin luz art.)", 3: "Ocaso (con luz art.)",
+        4: "Noche (ilum. ON)", 5: "Noche (ilum. OFF)", 6: "Noche (sin ilum.)", 999: "Sin especificar"
+    },
+    CONDICION_FIRME: {
+        1: "Seco y limpio", 2: "Húmedo", 3: "Mojado", 4: "Hielo",
+        5: "Nieve", 6: "Gravilla", 7: "Aceite", 8: "Barro", 9: "Otros", 999: "Sin especificar"
+    }
+};
